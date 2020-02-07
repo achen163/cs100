e7b3fdfb091d18707f31b498caeb19b5c6328735
Cyoo9 <tyo002@ucr.edu>
2020-02-06 21:08:35 -0800

    modified exec

--- header/executioner.h
+++ header/executioner.h
@@ -2,10 +2,12 @@
 #define __EXECUTIONER_H__
 
 #include "tokenizer.h"
+#include <stdio.h>
+#include <unistd.h>
 
 using namespace std;
 
-class Executioner : Tokenizer {
+class Executioner : public Tokenizer {
 	public:
 		void manager(char*); //don't really know yet
 	private:
