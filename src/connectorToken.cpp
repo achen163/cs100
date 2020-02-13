831252c5587ed45634e58dad9f08bd43a5c1bd8d
achen163 <achen163@ucr.edu>
2020-02-12 23:25:11 -0800

    Updating connector Token

--- src/connectorToken.cpp
+++ src/connectorToken.cpp
@@ -1,10 +1,20 @@
 #include "../header/connectorToken.h"
 #include <iostream>
 
-bool ConnectorToken::evaluate(char* arr[]) {
-	Executioner e = Executioner();
-	if(e.execute(arr) == 1)
+bool ConnectorToken::evaluate() {
+	Executioner exe;
+	char* array [v3.size()];
+	for(unsigned i = 0; i < v3.size(); ++i) {
+		array[i] = v3.at(i);
+	}
+	if(exe.execute(array) == 1)
 		return false;
 	return true;
 }
 
+char* ConnectorToken::stringify() {
+	for(unsigned i = 0; i < v2.size(); i++) {
+		v3.at(i) = (char*)v2.at(i).c_str();
+
+	
+}
