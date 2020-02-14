082c2fac3d7666cd1624a1fc39ed436dfa659b5e
Cyoo9 <tyo002@ucr.edu>
2020-02-13 20:42:31 -0800

    modified executioner

--- src/executioner.cpp
+++ src/executioner.cpp
@@ -3,23 +3,25 @@
 
 int Executioner::execute(char* args[]) {
 	pid_t pid = fork();
+
 	if(pid < 0) {
 		perror("forking failed");
 	}
 	if(pid == 0) {
 		cout << "running child process" << pid << endl;
 		if(execvp(args[0], args) == -1) {
-			perror("execution error. command not found");
+			const char* var = args[0];
+                        string cmdString(var);
+			string errorMsg = "-bash: " + cmdString + ": command not found";
+			perror(errorMsg.c_str());
 			exit(1);
 		}
 	} 
 	if(pid > 0) {
 		if(waitpid(-1, NULL, 0) == -1) 
-			perror("wait for child to finish");
-		else {
-			cout << "-bash: " + args[0] + ": command not found" << endl;
-			return 1;
-		}
-	}
+			perror("wait for child to finish"); 
+		
+	}	
 	return 0; // execvp is sucessful
+	
 }
