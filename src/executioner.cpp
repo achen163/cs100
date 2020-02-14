69d346c55c875615e1dc3534e279435070a9e0ee
Cyoo9 <tyo002@ucr.edu>
2020-02-13 22:38:07 -0800

    changed src

--- src/executioner.cpp
+++ src/executioner.cpp
@@ -3,28 +3,22 @@
 
 int Executioner::execute(char* args[]) {
 	pid_t pid = fork();
-	int isValid = 0;
 	if(pid < 0) {
 		perror("forking failed");
 	}
 	if(pid == 0) {
 		cout << "running child process" << pid << endl;
-		if(execvp(args[0], args) == -1) {
-			const char* e = "";
-			perror(e);
-			isValid = 1;
+		if(execvp(args[0], args) == -1) 
 			exit(1);
-		}
+		return 0;
 	} 
 	if(pid > 0) {
-		if(waitpid(-1, NULL, 0) == -1) 	perror("wait for child to finish"); 
-		if(isValid == 1) {		
-			const char* var = args[0];
-                	string cmdString(var);
-                	string errorMsg = "-bash: " + cmdString + ": command not found";
-			return 1;
-		}
-		return 0;
+		if(waitpid(-1, NULL, 0) == -1) 	perror("wait for child to finish"); 		
+		const char* var = args[0];
+               	string cmdString(var);
+               	string errorMsg = "-bash: " + cmdString + ": command not found";
+		return 1;
+	
 	}	
 	 // execvp is sucessful
 	
