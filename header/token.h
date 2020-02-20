098adb7ffe8dd14fff4e6f7bcb3035900a3e423b
achen163 <achen163@ucr.edu>
2020-02-19 22:02:38 -0800

    Updated token header

--- header/token.h
+++ header/token.h
@@ -13,7 +13,7 @@ class Token {
                 virtual void setLeft(Token*) = 0;
                 virtual void setRight(Token*) = 0;
                 virtual string item() = 0;
-                virtual bool evaluate(int, int) = 0;
+                virtual bool evaluate() = 0;
         private:
                 Token* left;
                 Token* right;
