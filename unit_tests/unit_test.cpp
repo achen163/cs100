2e9cf7d9774655cd974770816c4f52534c905bc1
Cyoo9 <tyo002@ucr.edu>
2020-02-26 21:09:24 -0800

    added some unit tests

--- unit_tests/unit_test.cpp
+++ unit_tests/unit_test.cpp
@@ -1,13 +1,20 @@
 #include "gtest/gtest.h"
-#include "../header/connectorToken.h"
-
+#include "../src/cmd.cpp"
 int main(int argc, char **argv) {
    ::testing::InitGoogleTest(&argc, argv);
    return RUN_ALL_TESTS();
 }
 
-/*TEST(ConnectorTest, EchoHello) {
-	ConnectorToken* test = new ConnectorToken("echo hello");
+TEST(executeTest, TestEchoHello) {
+	vector<string> sample;
+	sample.push_back("echo");
+	sample.push_back("hello");
+	Cmd* c = new Cmd(sample);
+	EXPECT_EQ(c->evaluate(), true);
+}
 	
-}*/
+	
+	
+
+
 
